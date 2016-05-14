36987980a3b678caa586a91db773e72993592555
Will <doggy.huang@gmail.com>
2016-05-14 15:14:51 +0800

    09 練習用 Native SQL 外加自定義 Model 來接收查詢結果

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace EFConsole
 {
+    public class DeptCourseCount
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+        public int CourseCount { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -13,16 +19,46 @@ namespace EFConsole
             using (var db = new ContosoUniversityEntities())
             {
                 //EF基本操作練習(db);
+
+                //var one = db.Course.Include("Department").FirstOrDefault(p => p.CourseID == 1);
+                //Console.WriteLine(one.Title + "\t" + one.Department.Name);
+
+
+                //foreach (var dept in db.Department)
+                //{
+                //    Console.WriteLine(dept.Name);
+
+                //    foreach (var course in dept.Course)
+                //    {
+                //        Console.WriteLine("\t" + course.Title);
+                //    }
+                //}
+
+                var sql = @"SELECT Department.DepartmentID, Department.Name, 
+                            (SELECT COUNT(*) FROM dbo.Course) as CourseCount
+                            FROM Department";
+                var data = db.Database.SqlQuery<DeptCourseCount>(sql);
+
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item.Name + "\t" + item.CourseCount);
+                }
+
+
+
+
             }
         }
 
         private static void EF基本操作練習(ContosoUniversityEntities db)
         {
+            db.Configuration.ProxyCreationEnabled = false;
+
             // 讀取所有 dbo.Course 中的資料
-            //foreach (var item in db.Course)
-            //{
-            //    Console.WriteLine(item.Title);
-            //}
+            foreach (var item in db.Course)
+            {
+                Console.WriteLine(item.Title);
+            }
 
             // 用 LINQ 篩選資料
             //var data = from p in db.Course
@@ -33,15 +69,15 @@ namespace EFConsole
             //    Console.WriteLine(item.Title);
             //}
 
-            // 新增 Course 資料
-            var c = new Course()
-            {
-                Title = "Git Test 2",
-                Credits = 4,
-                DepartmentID = 1
-            };
-            //c.Department = db.Department.Find(2);
-            db.Course.Add(c);
+            //// 新增 Course 資料
+            //var c = new Course()
+            //{
+            //    Title = "Git Test 2",
+            //    Credits = 4,
+            //    DepartmentID = 1
+            //};
+            ////c.Department = db.Department.Find(2);
+            //db.Course.Add(c);
 
             // 取得 Inserted ID
             //Console.WriteLine(c.CourseID);
@@ -50,14 +86,14 @@ namespace EFConsole
             //Console.ReadLine();
 
             // 刪除一筆資料
-            db.Course.Remove(db.Course.Find(12));
+            //db.Course.Remove(db.Course.Find(12));
 
-            // 批次更新資料
-            foreach (var item in db.Course)
-            {
-                item.Credits += 1;
-            }
-            db.SaveChanges();
+            //// 批次更新資料
+            //foreach (var item in db.Course)
+            //{
+            //    item.Credits += 1;
+            //}
+            //db.SaveChanges();
         }
     }
 }
