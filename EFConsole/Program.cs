6314dc7201b676607d6af7aa211a11d59d86c41e
Will <doggy.huang@gmail.com>
2016-05-14 13:17:17 +0800

    EF基本操作練習

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -10,6 +10,54 @@ namespace EFConsole
     {
         static void Main(string[] args)
         {
+            using (var db = new ContosoUniversityEntities())
+            {
+                //EF基本操作練習(db);
+            }
+        }
+
+        private static void EF基本操作練習(ContosoUniversityEntities db)
+        {
+            // 讀取所有 dbo.Course 中的資料
+            //foreach (var item in db.Course)
+            //{
+            //    Console.WriteLine(item.Title);
+            //}
+
+            // 用 LINQ 篩選資料
+            //var data = from p in db.Course
+            //           where p.Title != Guid.NewGuid().ToString()
+            //           select p;
+            //foreach (var item in data)
+            //{
+            //    Console.WriteLine(item.Title);
+            //}
+
+            // 新增 Course 資料
+            var c = new Course()
+            {
+                Title = "Git Test 2",
+                Credits = 4,
+                DepartmentID = 1
+            };
+            //c.Department = db.Department.Find(2);
+            db.Course.Add(c);
+
+            // 取得 Inserted ID
+            //Console.WriteLine(c.CourseID);
+            //db.SaveChanges();
+            //Console.WriteLine(c.CourseID);
+            //Console.ReadLine();
+
+            // 刪除一筆資料
+            db.Course.Remove(db.Course.Find(12));
+
+            // 批次更新資料
+            foreach (var item in db.Course)
+            {
+                item.Credits += 1;
+            }
+            db.SaveChanges();
         }
     }
 }
